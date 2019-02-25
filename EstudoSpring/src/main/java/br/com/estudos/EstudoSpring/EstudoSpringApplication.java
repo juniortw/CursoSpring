b2a2b578a303bef3b8ce30c0c94d52924875da6c
Amauri Junior <junior.tw@hotmail.com>
2019-02-25 12:49:36 -0300

    Inicio projeto

---
+++ EstudoSpring/src/main/java/br/com/estudos/EstudoSpring/EstudoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.EstudoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringApplication.class, args);
+	}
+
+}

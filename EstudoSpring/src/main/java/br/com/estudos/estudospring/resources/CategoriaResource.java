b9778dcd56242a564fe299a08a79ded8d6357cde
Amauri Junior <junior.tw@hotmail.com>
2019-02-25 15:22:02 -0300

    Criando a primeira classe de dominio

--- EstudoSpring/src/main/java/br/com/estudos/estudospring/resources/CategoriaResource.java
+++ EstudoSpring/src/main/java/br/com/estudos/estudospring/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package br.com.estudos.estudospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.estudos.estudospring.bean.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String lsitar() {
-		return "REST esta funcionando";
+	public List<Categoria> lsitar() {
+		
+		Categoria c1 = new Categoria(1, "Informatica");
+		Categoria c2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(c1);
+		list.add(c2);
+		
+		
+		return list;
 	}
 
 }

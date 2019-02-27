7abc7af22d4ad8f0b2656a23dd723d6e49d35fcb
Amauri Junior <junior.tw@hotmail.com>
2019-02-27 11:14:03 -0300

    Inclusao do Lombok no projeto e associacao Produtos_Categorias

--- EstudoSpring/src/main/java/br/com/estudos/EstudoSpring/EstudoSpringApplication.java
+++ EstudoSpring/src/main/java/br/com/estudos/EstudoSpring/EstudoSpringApplication.java
@@ -1,7 +1,5 @@
 package br.com.estudos.estudospring;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.estudos.estudospring.bean.Categoria;
+import br.com.estudos.estudospring.bean.Produto;
 import br.com.estudos.estudospring.repositories.CategoriaRepository;
+import br.com.estudos.estudospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class EstudoSpringApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository catRepository;
+	
+	@Autowired
+	ProdutoRepository prodRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSpringApplication.class, args);
@@ -27,7 +30,20 @@ public class EstudoSpringApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.setProdutos(Arrays.asList(p1,p2,p3));
+		cat2.setProdutos(Arrays.asList(p2));
+		
+		p1.setCategorias(Arrays.asList(cat1));
+		p2.setCategorias(Arrays.asList(cat1,cat2));
+		p3.setCategorias(Arrays.asList(cat1));
+		
+		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		prodRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 	
 	
--- EstudoSpring/src/main/java/br/com/estudos/estudospring/bean/Produto.java
+++ EstudoSpring/src/main/java/br/com/estudos/estudospring/bean/Produto.java
@@ -1,35 +1,44 @@
 package br.com.estudos.estudospring.bean;
 
-import java.util.List;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {	
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+    private Double preco;
+    
+    @ManyToMany
+    @JoinTable(name = "PRODUTO_CATEGORIA",
+    		joinColumns = @JoinColumn(name = "produto_id"),
+    		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+    private List<Categoria> categorias = new ArrayList<>();
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-
-	public Categoria() {}
-
-	public Categoria(Integer id, String nome) {
+    public Produto() {}    
+    
+	public Produto(Integer id, String nome, Double preco) {
+		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	@Override
@@ -48,7 +57,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -56,5 +65,6 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-
-}
+    
+    
+}   

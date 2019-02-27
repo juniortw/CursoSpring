208391ac61d697a25dd11e514e67c9edd7b71be8
Amauri Junior <junior.tw@hotmail.com>
2019-02-27 12:40:13 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ EstudoSpring/src/main/java/br/com/estudos/estudospring/resources/exception/StandardError.java
@@ -0,0 +1,24 @@
+package br.com.estudos.estudospring.resources.exception;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class StandardError implements Serializable{	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status;
+	private String msg;
+	private Long timeStamp;
+	
+	public StandardError(Integer status, String msg, Long timeStamp) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+	
+	
+
+}
--- EstudoSpring/src/main/java/br/com/estudos/estudospring/services/CategoriaService.java
+++ EstudoSpring/src/main/java/br/com/estudos/estudospring/services/CategoriaService.java
@@ -7,16 +7,19 @@ import org.springframework.stereotype.Service;
 
 import br.com.estudos.estudospring.bean.Categoria;
 import br.com.estudos.estudospring.repositories.CategoriaRepository;
+import br.com.estudos.estudospring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {		
-		Optional<Categoria> obj = repo.findById(id);		
-		return obj.orElse(null);
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado com id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
 
 }

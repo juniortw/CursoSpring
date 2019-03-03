28435452539625308dca504058764173c0168368
Amauri Junior <junior.tw@hotmail.com>
2019-03-03 20:22:21 -0300

    Criacao de novos Beans, Estado e Cidade

--- EstudoSpring/src/main/java/br/com/estudos/EstudoSpring/EstudoSpringApplication.java
+++ EstudoSpring/src/main/java/br/com/estudos/EstudoSpring/EstudoSpringApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.estudos.estudospring.bean.Categoria;
+import br.com.estudos.estudospring.bean.Cidade;
+import br.com.estudos.estudospring.bean.Estado;
 import br.com.estudos.estudospring.bean.Produto;
 import br.com.estudos.estudospring.repositories.CategoriaRepository;
+import br.com.estudos.estudospring.repositories.CidadeRepository;
+import br.com.estudos.estudospring.repositories.EstadoRepository;
 import br.com.estudos.estudospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,10 +21,16 @@ public class EstudoSpringApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository catRepository;
-	
+
 	@Autowired
 	ProdutoRepository prodRepository;
-	
+
+	@Autowired
+	CidadeRepository cidadeRepository;
+
+	@Autowired
+	EstadoRepository estadoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSpringApplication.class, args);
 	}
@@ -29,24 +39,33 @@ public class EstudoSpringApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.setProdutos(Arrays.asList(p1,p2,p3));
+
+		cat1.setProdutos(Arrays.asList(p1, p2, p3));
 		cat2.setProdutos(Arrays.asList(p2));
-		
+
 		p1.setCategorias(Arrays.asList(cat1));
-		p2.setCategorias(Arrays.asList(cat1,cat2));
+		p2.setCategorias(Arrays.asList(cat1, cat2));
 		p3.setCategorias(Arrays.asList(cat1));
-		
-		
+
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
-		prodRepository.saveAll(Arrays.asList(p1,p2,p3));
+		prodRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.setCidades(Arrays.asList(c1));
+		est2.setCidades(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
-	
-	
 
 }
- 
---
+++ EstudoSpring/src/main/java/br/com/estudos/estudospring/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package br.com.estudos.estudospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.estudos.estudospring.bean.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {}
